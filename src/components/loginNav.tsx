1a8c8e03b11ba75a815e71c6d9f1dd06241a2e2e
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2023-01-26 14:21:31 -0800

    starting with login nav/login auth --- setting up

---
+++ src/components/loginNav.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Flex,
+  Heading,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Tooltip,
+  useColorMode,
+  IconButton,
+  useToast,
+} from "@chakra-ui/react";

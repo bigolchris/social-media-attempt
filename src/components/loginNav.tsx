b2b678ca93d9a06f6aa4a797a8fca5e48f58a35d
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2023-01-26 15:35:44 -0800

    added a login nav functions --- do not have it printing --- will with chakra

--- src/components/loginNav.tsx
+++ src/components/loginNav.tsx
@@ -13,7 +13,57 @@ import {
   useToast,
 } from "@chakra-ui/react";
 
+import { ImSearch } from "react-icons/im";
+import { FaMoon, FaSun, FaUser } from "react-icons/fa";
+
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { doc, setDoc, getFirestore } from "firebase/firestore";
+
+const LoginNav = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const toast = useToast();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleLogin = () => {
+    setLoading(true);
+    signInWithPopup(auth, provider)
+      .then(async (result) => {
+        await setDoc(doc(db, "users", result?.user?.uid), {
+          userName: result?.user?.displayName,
+          bio: "big ol dongs",
+          uid: result?.user?.uid,
+          profilePic: result?.user?.photoURL,
+          email: result?.user?.email,
+        })
+          .then(() => {
+            setLoading(false);
+            navigate("/");
+          })
+          .catch(() => {
+            toast({
+              title: "Error",
+              description: "Something went wrong",
+              status: "error",
+              duration: 5000,
+              isClosable: true,
+            });
+          });
+      })
+      .catch((err) => {
+        setLoading(false);
+        toast({
+          title: "Error",
+          description: err.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      });
+  };
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react/dist/chakra-provider";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );

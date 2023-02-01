07e8a20103d794a58925341e3e2c835833a60280
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2023-01-31 20:05:03 -0800

    setting up stuff --- login stuff

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,2 +1,21 @@
 import React, { useState, useEffect } from "react";
 import LoginNav from "../components/loginNav";
+
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Tooltip,
+  useToast,
+} from "@chakra-ui/react";
+
+import { AiOutlineFire } from "react-icons/ai";
+
+import { app } from "../firebase";
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+import { useNavigate } from "react-router-dom";
+
+import { doc, setDoc, getFirestore, serverTimestamp } from "firebase/firestore";

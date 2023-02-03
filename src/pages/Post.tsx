97fc1aa554a8a9f3ebf22cf277609a24246b8f15
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2023-02-03 15:38:43 -0800

    more stuff

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -31,3 +31,70 @@ import {
   BsHeartFill,
   BsBookmarkFill,
 } from "react-icons/bs";
+
+import { MdOutlineReportProblem } from "react-icons/md";
+import { format } from "timeago.js";
+import { app } from "../firebase";
+import { getAuth } from "firebase/auth";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from "@chakra-ui/react";
+
+import {
+  doc,
+  deleteDoc,
+  getFirestore,
+  setDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  getDoc,
+  addDoc,
+  DocumentData,
+  query,
+  updateDoc,
+} from "firebase/firestore";
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+} from "@chakra-ui/react";
+
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+
+type Props = {
+  posts?: {
+    caption: string;
+    createdAt: string;
+    image: string;
+    userName: string;
+    userId: string;
+    userPfp: string;
+    id: string;
+  };
+};

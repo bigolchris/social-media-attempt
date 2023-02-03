e3ef18ad97df1e33163167c055a0bbb7d1d661cd
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2023-02-03 15:31:31 -0800

    getting post component started

---
+++ src/pages/Post.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+import {
+  Flex,
+  Heading,
+  Avatar,
+  IconButton,
+  Image,
+  Tooltip,
+  Button,
+  useDisclosure,
+  useToast,
+  Input,
+  SkeletonCircle,
+  SkeletonText,
+  Skeleton,
+  Radio,
+  useColorMode,
+} from "@chakra-ui/react";
+
+import {
+  BiDotsVerticalRounded,
+  BiCommentDetail,
+  BiEdit,
+  BiTrash,
+} from "react-icons/bi";
+
+import {
+  BsHeart,
+  BsBookmark,
+  BsHeartFill,
+  BsBookmarkFill,
+} from "react-icons/bs";
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -14,8 +14,97 @@ import { AiOutlineFire } from "react-icons/ai";
 
 import { app } from "../firebase";
 
+import { doc, setDoc, getFirestore, serverTimestamp } from "firebase/firestore";
+
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 import { useNavigate } from "react-router-dom";
 
-import { doc, setDoc, getFirestore, serverTimestamp } from "firebase/firestore";
+const LoginPage = () => {
+  useEffect(() => {
+    document.title = "PLACE_HOLDER | Login";
+  }, []);
+  const toast = useToast();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleLogin = () => {
+    setLoading(true);
+    signInWithPopup(auth, provider)
+      .then(async (result) => {
+        await setDoc(doc(db, "users", result?.user?.uid), {
+          userName: result?.user?.displayName,
+          bio: "big ol dongs",
+          uid: result?.user?.uid,
+          profilePic: result?.user?.photoURL,
+          email: result?.user?.email,
+          createdAt: serverTimestamp(),
+        })
+          .then(() => {
+            setLoading(false);
+            navigate("/");
+          })
+          .catch(() => {
+            toast({
+              title: "Error",
+              description: "Something went wrong",
+              status: "error",
+              duration: 5000,
+              isClosable: true,
+            });
+          });
+      })
+      .catch((err) => {
+        setLoading(false);
+        toast({
+          title: "Error",
+          description: "Something went wrong",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      });
+  };
+
+  return (
+    <Flex overflow="hidden" flexDirection="column" height="100vh" width="100vw">
+      <LoginNav />
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        width="100vw"
+        height="73vh"
+        overflow="hidden"
+      >
+        <Flex flexDirection="column" gap="1.4rem">
+          <Flex flexDirection="column" alignItems="center">
+            <Heading as="h4" size="xl">
+              Make Friends
+            </Heading>
+            <Heading as="h4" size="xl">
+              PLACE_HOLDER
+            </Heading>
+          </Flex>
+          <Tooltip label="idk man" openDelay={350}>
+            <Button
+              isLoading={loading}
+              leftIcon={<AiOutlineFire />}
+              colorScheme="purple"
+              variant="solid"
+              borderRadius={24}
+              size="lg"
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+          </Tooltip>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default LoginPage;
